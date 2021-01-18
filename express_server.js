8aaaedb33605199a13da48e375db8388e872fe95
May C <maycheongs@gmail.com>
2021-01-18 22:00:06 +0000

    render urls page with dead short links

--- express_server.js
+++ express_server.js
@@ -1,23 +1,38 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs');
 
 const PORT = 8080;
 
-const urlDatabase = [{                       //an object (i.e. object/array)
+const urlDatabase = {                       //an object (i.e. object/array)
   "b2xVn2" : "http://lighthouselabs.ca",
   "9sm5xk" : "http://google.com"
-}];
+};
 
-app.get('/', (req, res) => {
-  res.send('Hello');                            //works like .write
-});
+// //home page
+// app.get('/', (req, res) => {
+//   //res.send('Hello');                           default express- works like .write
+//   res.render('pages/index');
+// });
+
+// //about page
+// app.get('/about',(req, res) => {
+//   res.render('pages/about');
+// });
+
 
 app.get('/urls', (req, res) => {
-  res.json(urlDatabase);              //.json does JSON.stringify on an object automatically
+  const templateVars = { urls: urlDatabase}
+  res.render('urls_index', templateVars);              //.json does JSON.stringify on an object automatically
 });
+
+app.get('/hello', (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n")
+})
 
 
 app.listen(PORT, () => {
   console.log(`Test app listening on port ${PORT}`);
 });
+
 
