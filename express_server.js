4ff8007698585d2a059e0735c6ba78f460894e12
May C <maycheongs@gmail.com>
2021-01-18 20:17:41 +0000

    response.json example

--- express_server.js
+++ express_server.js
@@ -3,14 +3,19 @@ const app = express();
 
 const PORT = 8080;
 
-const urlDatabase = {
+const urlDatabase = [{                       //an object (i.e. object/array)
   "b2xVn2" : "http://lighthouselabs.ca",
   "9sm5xk" : "http://google.com"
-};
+}];
 
 app.get('/', (req, res) => {
-  res.send('Hello');
+  res.send('Hello');                            //works like .write
 });
+
+app.get('/urls', (req, res) => {
+  res.json(urlDatabase);              //.json does JSON.stringify on an object automatically
+});
+
 
 app.listen(PORT, () => {
   console.log(`Test app listening on port ${PORT}`);
