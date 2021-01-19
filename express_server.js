e87c4873305fedb9cc9f63acd55c8a74baac3824
May C <maycheongs@gmail.com>
2021-01-19 02:30:49 +0000

    added some comments

--- express_server.js
+++ express_server.js
@@ -1,3 +1,5 @@
+
+//starter code
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -5,7 +7,7 @@ const bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
-
+//helper function
 const generateRandomString = num => {
   let string = '';
   while (num > 0) {
@@ -19,30 +21,20 @@ const generateRandomString = num => {
   return string;
 }
 
+//And we begin!
 const PORT = 8080;
-
-const urlDatabase = {                       //an object (i.e. object/array)
+const urlDatabase = {
   "b2xVn2" : "http://lighthouselabs.ca",
   "9sm5xk" : "http://google.com"
 };
 
-// //home page
-// app.get('/', (req, res) => {
-//   //res.send('Hello');                           default express- works like .write
-//   res.render('pages/index');
-// });
-
-// //about page
-// app.get('/about',(req, res) => {
-//   res.render('pages/about');
-// });
-
-
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase}
-  res.render('urls_index', templateVars);              //.json does JSON.stringify on an object automatically
+  res.render('urls_index', templateVars);
 });
 
+//FORM from /urls_new -> request to url/ with input body, input name = longURL.
+//if long url entered exists, redirect to existing /urls/shortURL, else create and redirect.
 app.post('/urls', (req, res) => {
   let site = req.body.longURL;
   for (let short in urlDatabase) {
@@ -57,20 +49,22 @@ app.post('/urls', (req, res) => {
   res.redirect(`/urls/${newId}`);
 })
 
-app.get('/hello', (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n")
-})
+// app.get('/hello', (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n")
+// })
 
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
+// :ID generates a object key 'ID' on the inbuilt req.params object with the value as per request.
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   console.log(req.params)
   res.render('urls_show', templateVars);
 })
 
+//redirect to actual url using the short form.
 app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];  
   res.redirect(longURL);
